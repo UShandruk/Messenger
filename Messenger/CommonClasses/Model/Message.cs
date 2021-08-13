7012e8d02db5f5f2e5e75c84db68057fe6074a8e
UShandruk <ulyana.shandruk@gmail.com>
2021-08-13 16:16:01 +0300

    Модель - первый коммит
    Модель (класс Message) вынесен в отдельный проект, чтобы не дублировать его в API и ClientDesctop

---
+++ Messenger/CommonClasses/Model/Message.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClasses.Model
+{
+    /// <summary>
+    /// Сообщение
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Уникальный идентификатор пользователя
+        /// </summary>
+        public int UId { get; set; }
+
+        /// <summary>
+        /// Дата и время отправки сообщения
+        /// </summary>
+        public DateTime Datetime { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Текст сообщения
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="uId">Уникальный идентификатор пользователя</param>
+        /// <param name="userName">Имя пользователя</param>
+        /// <param name="text">Текст сообщения</param>
+        public Message(int uId, string userName, string text)
+        {
+            UId = uId;
+            Datetime = DateTime.Now;
+            Username = userName;
+            Text = text;
+        }
+
+        public override String ToString()
+        {
+            String stringMessage = this.Datetime.ToString("") + " " + this.Username + " " + Environment.NewLine + this.Text + Environment.NewLine;
+            return stringMessage;
+        }
+    }
+}
